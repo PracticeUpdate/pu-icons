ea245577598862de4e0b4bc3dfa35b54989d9402
Liccy Fuentes <liccyfuentes@gmail.com>
2014-07-01 17:24:07 -0400

    update for icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,136 +1,113 @@
-/*global module:false*/
 module.exports = function(grunt) {
-
+  // require it at the top and pass in the grunt instance
+  require('time-grunt')(grunt);
+  
   // Project configuration.
   grunt.initConfig({
-    // Metadata.
     pkg: grunt.file.readJSON('package.json'),
+    
     banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+
     // Task configuration.
-    concat: {
+    paths: {
+      dest: 'dest',
+      dist: 'dist',
+      src: 'src'
+    },
+
+		assemble: {
       options: {
-        banner: '<%= banner %>',
-        stripBanners: true
+        pkg: '<%= pkg %>',
+        data: '<%= paths.dist %>/_data/*.json',
       },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
+			dev: {
+				files: [{
+					expand: true,
+					cwd: '<%= paths.dist %>/_page/',
+					src: ['*.hbs'],
+					dest: '<%= paths.dest %>/page/',
+					ext: '.html'
+				}]
+			}
+		},
+
+		svgstore: {
+			options: {},
+			default : {
+				files: {
+					'<%= paths.dest %>/dest.svg': ['<%= paths.src %>/*.svg'],
+					'<%= paths.dest %>/dest.ai': ['<%= paths.src %>/*.ai'],
+				},
+			},
+		},
+
+		'svg-sprites': {
+			pu: {
+				options: {
+					spriteElementPath: '<%= paths.src %>',
+					spritePath: "<%= paths.dest %>/sprite/pu-sprite.svg",
+					cssPath: "<%= paths.dest %>/sprite/pu-sprite.css"
+				}
+			}
+		},
+
+		grunticon: {
+			myIcons: {
+				files: [{
+					expand: true,
+					cwd: '<%= paths.src %>',
+					src: ['*.svg', '*.png'],
+					dest: '<%= paths.dest %>/grunticons/'
+				}],
+				options: {
+						defaultWidth: "40px",
+						defaultHeight: "40px"
+				}
+			}
+		},
+
+		webfont: {
+			icons: {
+				src: '<%= paths.src %>/*.svg',
+				dest: '<%= paths.dest %>/webfonts',
+				options: {
+					//
+				}
+			}
+		},
+
     watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
-      }
+			svgstore: {
+				files: ['<%= paths.dest %>/dest.ai', '<%= paths.dest %>/dest.svg'],
+				tasks: "svgstore"
+			},
     },
-    font: {
-        all: {
-          // SVG files to read in
-          src: ['src/**/*.svg'],
 
-          // Location to output CSS variables
-          destCss: 'dist/css/icons.styl',
-
-          // Location to output fonts (expanded via brace expansion)
-          destFonts: 'dist/fonts/icons.{svg,woff,eot,ttf}',
-
-          // Multiple CSS outputs supported (generated .styl and .json files)
-          destCss: 'dist/pu-icons/font.{styl,json}',
-
-          // Alternative formats (1)
-          destCss:[
-            'dist/pu-icons/font.styl',
-            'dist/pu-icons/font.json'
-          ],
-          destFonts: [
-            'dist/pu-icons/font.svg',
-            'dist/pu-icons/font.woff',
-            'dist/pu-icons/font.eot'
-          ],
-
-          // Alternative formats (2)
-          destFonts: {
-            // Override specific engines
-            json: 'dist/pu-icons/font.less',
-            styl: 'dist/pu-icons/font.json'
-          },
-          destFonts: {
-            // Override specific engines
-            'dev-svg': 'dist/pu-icons/font.svg',
-            woff: 'dist/pu-icons/font.waffles',
-            eot: 'dist/pu-icons/more.like.eof'
-          },
-
-          // OPTIONAL: Specify CSS format (inferred from destCss' extension by default)
-              // (stylus, less, scss, json)
-          cssFormat: 'json',
-
-          // Optional: Custom routing of font filepaths for CSS
-          cssRouter: function (fontpath) {
-            return 'mysubfolder/' + fontpath;
-          },
-
-          // Optional: Custom naming of font families for multi-task support
-          fontFamily: 'my-icon-font',
-
-          // OPTIONAL: Specify CSS options
-          cssOptions: {}
+    connect: {
+      options: {
+        hostname: '*',
+        port: 9001
+      },
+      server: {
+        options: {
+          base: '<%= paths.dev %>',
+          open: true,
+          keepalive: true,
+          livereload: true,
         }
       }
-    });
+    }
+  });
 
-  // These plugins provide necessary tasks.
-  // grunt.loadNpmTasks('grunt-contrib-concat');
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
-  // grunt.loadNpmTasks('grunt-contrib-qunit');
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
-  // grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-fontsmith');
+  //load npm tasks
+  require('load-grunt-tasks')(grunt);
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-
+  // Default task(s).
+  //grunt.registerTask('default', ['svgstore', 'watch']);
+  grunt.registerTask('default', ['svgstore', 'svg-sprites', 'grunticon:myIcons', 'webfont', 'assemble:dev']);
+	grunt.registerTask('preview', ['connect']);  
 };
